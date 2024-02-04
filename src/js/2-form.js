a035cf81a30f92ee6b0c2edbe28ce83366462bf3
vanisimov4 <144102084+vanisimov4@users.noreply.github.com>
2024-02-04 12:00:31 +0200

    task-2Finish

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,42 +1,50 @@
+const STORAGE_KEY = 'feedback-form-state';
+
 const form = document.querySelector('form.feedback-form');
-// const inputForm = from.querySelector('input');
-// const textareaForm = from.querySelector('textarea');
-// form.elements.email.value = 
-const localStorageKey = "feedback-form-state";
-form.elements.email.value = localStorage.getItem(localStorageKey) ?? "";
 
 form.addEventListener('input', handleFormInput);
 form.addEventListener('submit', handleFormSubmit);
 
-function handleFormInput(event) { 
-    const email = form.elements.email.value;
-    const message = form.elements.message.value;
-        const objForm = {
-            email,
-            message,
-    };
-    console.log(objForm);
-    saveToLS(localStorageKey, objForm);
+function handleFormInput() {
+  const email = form.elements.email.value.trim();
+  const message = form.elements.message.value.trim();
+  const objForm = {
+    email,
+    message,
+  };
+  saveToLS(STORAGE_KEY, objForm);
 }
 
-function handleFormSubmit(event) { 
-    event.preventDefault();
-    console.log(event.target.elements.email.value);
-    console.log(event.target.elements.message.value);
-    localStorage.removeItem(localStorageKey);
-    form.reset();
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  if (form.elements.email.value === '' || form.elements.message.value === '') {
+    return alert('All form fields must be filled in');
+  }
+  const data = loadFromLS(STORAGE_KEY);
+  console.log(data);
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
 }
 
 function saveToLS(key, value) {
-    const zip = JSON.stringify(value);
-    localStorage.setItem(key, zip);
+  const zip = JSON.stringify(value);
+  localStorage.setItem(key, zip);
 }
 
 function loadFromLS(key) {
-    const zip = localStorage.getItem(key);
-    try {
-        return JSON.parse(zip);
-    } catch {
-        return zip;
-    }
+  const zip = localStorage.getItem(key);
+  try {
+    return JSON.parse(zip);
+  } catch {
+    return zip;
+  }
 }
+
+function init() {
+  const data = loadFromLS(STORAGE_KEY) || {};
+  form.elements.email.value = data.email || '';
+  form.elements.message.value = data.message || '';
+}
+
+init();
